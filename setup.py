a6b83cb9531ee2a064d6a025c50ecd7ebc1c6435
vgerasimov <vladgerasimov94@yandex.ru>
2023-07-29 16:17:21 +0300

    Создан пакет restclient

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+REQUIRES = [
+    "requests",
+    "structlog",
+    "curlify",
+    "allure-pytest",
+]
+
+setup(
+    name='restclient',
+    version='0.0.1',
+    packages=['restclient'],
+    url='https://github.com/vladgerasimov94/restclient.git',
+    license='MIT',
+    author='Vladislav Gerasimov',
+    author_email='-',
+    install_requires=REQUIRES,
+    description='restclient with allure and logger'
+)
